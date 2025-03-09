d9da091ee56b8bfd556d55c704c5eff3ec11ef26
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-03-09 10:34:31 +0700

    Update resources.js

--- mario/Enjine/resources.js
+++ mario/Enjine/resources.js
@@ -1,11 +1,7 @@
-/**
-	Represents a simple static sprite.
-	Code by Rob Kleffner, 2011
-*/
-
 Enjine.Resources = {
     Images: {},
     Sounds: {},
+    Muted: false, // Tambahkan properti Muted
 
     Destroy: function() {
         delete this.Images;
@@ -17,30 +13,30 @@ Enjine.Resources = {
     //Images
     AddImage: function(name, src) {
         var tempImage = new Image();
-		this.Images[name] = tempImage;
+        this.Images[name] = tempImage;
         tempImage.src = src;
         return this;
-	},
-	
-	AddImages: function(array) {
-		for (var i = 0; i < array.length; i++) {
+    },
+    
+    AddImages: function(array) {
+        for (var i = 0; i < array.length; i++) {
             var tempImage = new Image();
             this.Images[array[i].name] = tempImage;
             tempImage.src = array[i].src;
         }
         return this;
-	},
-	
-	ClearImages: function() {
-		delete this.Images;
+    },
+    
+    ClearImages: function() {
+        delete this.Images;
         this.Images = new Object();
         return this;
-	},
-	
-	RemoveImage: function(name) {
-		delete this.Images[name];
-		return this;
-	},
+    },
+    
+    RemoveImage: function(name) {
+        delete this.Images[name];
+        return this;
+    },
     
     //***********************/
     //Sounds
@@ -48,10 +44,10 @@ Enjine.Resources = {
         this.Sounds[name] = [];
         this.Sounds[name].index = 0;
         if (!maxChannels) {
-        	maxChannels = 3;
+            maxChannels = 3;
         }
         for (var i = 0; i < maxChannels; i++) {
-        	this.Sounds[name][i] = new Audio(src);	
+            this.Sounds[name][i] = new Audio(src);    
         }
         return this;
     },
@@ -68,52 +64,75 @@ Enjine.Resources = {
     },
     
     PlaySound: function(name, loop) {
-    	if (this.Sounds[name].index >= this.Sounds[name].length) {
-    		this.Sounds[name].index = 0;	
-    	}
-    	if (loop) {
-    		this.Sounds[name][this.Sounds[name].index].addEventListener("ended", this.LoopCallback, false);
-    	}
-    	this.Sounds[name][this.Sounds[name].index++].play();
-    	return this.Sounds[name].index;
+        if (this.Muted) return; // Jangan mainkan suara jika muted
+        if (!this.Sounds[name]) return; // Pastikan suara ada
+
+        if (this.Sounds[name].index >= this.Sounds[name].length) {
+            this.Sounds[name].index = 0;    
+        }
+        if (loop) {
+            this.Sounds[name][this.Sounds[name].index].addEventListener("ended", this.LoopCallback, false);
+        }
+        this.Sounds[name][this.Sounds[name].index++].play();
+        return this.Sounds[name].index;
     },
     
     PauseChannel: function(name, index) {
-    	if (!this.Sounds[name][index].paused) {
-    		this.Sounds[name][index].pause();
-    	}
-    	return this;
+        if (!this.Sounds[name][index].paused) {
+            this.Sounds[name][index].pause();
+        }
+        return this;
     },
     
     PauseSound: function(name) {
-    	for (var i = 0; i < this.Sounds[name].length; i++) {
-    		if (!this.Sounds[name][i].paused) {
-    			this.Sounds[name][i].pause();
-    		}
-    	}
-    	return this;
+        for (var i = 0; i < this.Sounds[name].length; i++) {
+            if (!this.Sounds[name][i].paused) {
+                this.Sounds[name][i].pause();
+            }
+        }
+        return this;
     },
     
     ResetChannel: function(name, index) {
-    	this.Sounds[name][index].currentTime = 0;
-    	this.StopLoop(name, index);
-    	return this;
+        this.Sounds[name][index].currentTime = 0;
+        this.StopLoop(name, index);
+        return this;
     },
     
     ResetSound: function(name) {
-    	for (var i = 0; i < this.Sounds[name].length; i++) {
-    		this.Sounds[name].currentTime = 0;
-    		this.StopLoop(name, i);
-    	}
-    	return this;
+        for (var i = 0; i < this.Sounds[name].length; i++) {
+            this.Sounds[name][i].currentTime = 0;
+            this.StopLoop(name, i);
+        }
+        return this;
     },
     
     StopLoop: function(name, index) {
-    	this.Sounds[name][index].removeEventListener("ended", this.LoopCallback, false);	
+        this.Sounds[name][index].removeEventListener("ended", this.LoopCallback, false);    
     },
     
     LoopCallback: function() {
-    	this.currentTime = -1;
-    	this.play();
+        this.currentTime = -1;
+        this.play();
+    },
+
+    // Tambahkan fungsi untuk mengatur mute
+    SetMute: function(mute) {
+        this.Muted = mute;
+        if (mute) {
+            this.StopAllSounds(); // Hentikan semua suara jika mute
+        }
+    },
+
+    // Tambahkan fungsi untuk menghentikan semua suara
+    StopAllSounds: function() {
+        for (var sound in this.Sounds) {
+            if (this.Sounds[sound]) {
+                for (var i = 0; i < this.Sounds[sound].length; i++) {
+                    this.Sounds[sound][i].pause();
+                    this.Sounds[sound][i].currentTime = 0;
+                }
+            }
+        }
     }
 };
